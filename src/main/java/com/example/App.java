0976e60eac8a447ea7fa759bf3b18100d9184d0c
Vivekkulkarni521 <vivekkulkarni521@gmail.com>
2026-07-17 23:00:57 +0530

    App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello from Maven CI/CD Pipeline");
+
+        while (true) {
+            Thread.sleep(10000);
+        }
     }
 }
